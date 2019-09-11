0be4a71de334f659a90dbd78ea1d704db056067d
Murilo Eduardo dos Santos <muriloeduardoooooo@gmail.com>
2019-09-10 22:11:00 -0300

    change avatar icon default size

--- resources/js/components/profile-preview-avatar.vue
+++ resources/js/components/profile-preview-avatar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div class="preview-image" @click="openUploadIMage">
+		<div class="preview-image text-center" @click="openUploadIMage">
 			<img
 				v-if="user.avatar_url"
 				class="img-fluid avatar"
@@ -8,7 +8,7 @@
 				v-bind:alt="user.name"
 				v-bind:title="user.name"
 			/>
-			<i v-else class="fas fa-user-circle fa-4x"></i>
+			<i v-else class="fas fa-user-circle fa-6x"></i>
 		</div>
 		<input type="file" @change="onFileChange" name="avatar" ref="avatarFile" hidden />
 	</div>
