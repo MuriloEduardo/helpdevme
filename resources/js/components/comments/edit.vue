42bf75063809f4e1d7981fe17f24397cbcca951e
Murilo Eduardo dos Santos <muriloeduardoooooo@gmail.com>
2019-09-10 21:21:24 -0300

    adicionando a opcao de cancelar edicao de comentario

--- resources/js/components/comments/edit.vue
+++ resources/js/components/comments/edit.vue
@@ -1,44 +1,55 @@
 <template>
-	<form @submit.prevent="onSubmit">
-		<b-input-group class="border rounded-pill p-1 bg-white">
-			<b-form-input
-				class="border-0"
-				placeholder="Oi, eu sei solucionar isso!"
-				v-model="body"
-				autofocus
-				required
-			></b-form-input>
+	<div>
+		<form @submit.prevent="onSubmit">
+			<b-input-group class="border rounded-pill p-1 bg-white">
+				<b-form-input
+					class="border-0"
+					placeholder="Oi, eu sei solucionar isso!"
+					v-model="body"
+					autofocus
+					required
+				></b-form-input>
 
-			<b-input-group-append>
-				<b-dropdown
-					variant="outline-success rounded-pill mx-1"
-					v-b-tooltip.hover
-					title="Adicionar Orçamento"
-					right
-					slot="append"
-				>
-					<template slot="button-content">
-						<div v-if="!budget" class="d-flex align-items-center">
-							<i class="fas fa-dollar-sign mr-2"></i>
-							<small>Proposta</small>
-						</div>
-						<span v-else>{{ budget | currency }}</span>
-					</template>
-					<b-dropdown-form>
-						<b-form-group label="Orçamento" label-for="dropdown-form-budget" @submit.stop.prevent>
-							<b-input-group prepend="R$">
-								<b-form-input id="dropdown-form-budget" v-model="budget" placeholder="2,50"></b-form-input>
-							</b-input-group>
-						</b-form-group>
-					</b-dropdown-form>
-				</b-dropdown>
-				<b-button variant="primary" type="submit" class="rounded-pill" v-b-tooltip.hover title="Enviar">
-					<i v-if="!loading" class="fas fa-paper-plane"></i>
-					<b-spinner v-else small type="grow"></b-spinner>
-				</b-button>
-			</b-input-group-append>
-		</b-input-group>
-	</form>
+				<b-input-group-append>
+					<b-dropdown
+						variant="outline-success rounded-pill mx-1"
+						v-b-tooltip.hover
+						title="Adicionar Orçamento"
+						right
+						slot="append"
+					>
+						<template slot="button-content">
+							<div v-if="!budget" class="d-flex align-items-center">
+								<i class="fas fa-dollar-sign mr-2"></i>
+								<small>Proposta</small>
+							</div>
+							<span v-else>{{ budget | currency }}</span>
+						</template>
+						<b-dropdown-form>
+							<b-form-group label="Orçamento" label-for="dropdown-form-budget" @submit.stop.prevent>
+								<b-input-group prepend="R$">
+									<b-form-input id="dropdown-form-budget" v-model="budget" placeholder="2,50"></b-form-input>
+								</b-input-group>
+							</b-form-group>
+						</b-dropdown-form>
+					</b-dropdown>
+					<b-button
+						variant="primary"
+						type="submit"
+						class="rounded-pill"
+						v-b-tooltip.hover
+						title="Enviar"
+					>
+						<i v-if="!loading" class="fas fa-paper-plane"></i>
+						<b-spinner v-else small type="grow"></b-spinner>
+					</b-button>
+				</b-input-group-append>
+			</b-input-group>
+		</form>
+		<button class="btn btn-sm btn-link" @click="closeFormEdit">
+			<small>Cancelar</small>
+		</button>
+	</div>
 </template>
 <script>
 import { mapActions } from 'vuex';
@@ -54,6 +65,9 @@ export default {
 	},
 	methods: {
 		...mapActions('questions', ['addComment', 'closetFormEditComment']),
+		closeFormEdit() {
+			this.closetFormEditComment(this.question.id);
+		},
 		onSubmit() {
 			this.loading = true;
 
@@ -73,7 +87,7 @@ export default {
 						solid: true
 					});
 
-					this.closetFormEditComment(this.question.id);
+					this.closeFormEdit();
 				} else {
 					this.$bvToast.toast(
 						'Tente novamente de uma forma diferente!',
