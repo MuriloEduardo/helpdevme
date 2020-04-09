5c9e328729c746ca67374d3304d3f593cb26c0a4
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2020-04-09 02:27:25 -0300

    muda redirecionamento de cadastro de usuario para contabilizar meta do analytics

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
 	 *
 	 * @var string
 	 */
-	protected $redirectTo = '/questions';
+	protected $redirectTo = '/questions/create?meta=conta-criada';
 
 	/**
 	 * Create a new controller instance.
