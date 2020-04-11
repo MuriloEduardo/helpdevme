4fb68fd67c6b76c5ef30eaad301945f2778334a5
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2020-04-11 14:28:49 -0300

    melhora pagina de perfil de usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,37 +1,60 @@
-@extends('layouts.main')
-
-@section('title', $user->name . ' - Help Dev')
-
+@extends('layouts.main') @section('title', $user->name . ' - Help Dev')
 @section('content')
-<div class="row user-show">
-	<div class="col col-lg offset-lg-1">
-		<div class="row align-items-center">
-			<div class="col flex-grow-0">
-				@include('shared.avatar', ['user' => $user, 'icon_class' => 'fa-4x'])
-			</div>
-			<div class="col">
+<div class="row">
+	<div class="col-lg-10 m-auto">
+		<div class="row">
+			<div class="col-lg-4">
+				@include('shared.avatar', ['user' => $user, 'icon_class' =>
+				'fa-4x'])
 				<h1 class="font-weight-light">{{ $user->name }}</h1>
 				<p class="lead">{{ $user->email }}</p>
+				<div>
+					<h3 class="h6">Linguagens que domina</h3>
+					<div class="d-flex">
+						@foreach($user->tags as $tag)
+						<a
+							href="{{ route('tags.show', $tag) }}"
+							class="badge badge-primary badge-pill m-1"
+						>
+							<div class="d-flex align-items-center p-1">
+								<i
+									class="{{ $tag->image }} colored fa-lg mr-1"
+								></i>
+								<span>{{ $tag->title }}</span>
+							</div>
+						</a>
+						@endforeach
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="row">
 			<div class="col">
-				<h2 class="my-5">
-					{{ $user->talks->count() . ' ' . str_plural('ajuda', $user->talks->count()) . ' ' . str_plural('prestada', $user->talks->count()) }}
-				</h2>
-				@foreach ($user->talks as $talk)
-				@include('shared.questions.item', ['question' => $talk->question])
-				@endforeach
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<h2 class="my-5">
-					{{ $user->questions->count() . ' ' . str_plural('pedido', $user->questions->count()) }} de
-					socorro</h2>
-				@foreach ($user->questions as $question)
-				@include('shared.questions.item', ['question' => $question])
-				@endforeach
+				<div class="row">
+					<div class="col">
+						<div class="card card-body">
+							<h5>
+								<span
+									class="h1"
+									>{{ $user->talks->count() }}</span
+								>
+								<br />
+								{{ str_plural('ajuda', $user->talks->count()) . ' ' . str_plural('prestada', $user->talks->count()) }}
+							</h5>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card card-body">
+							<h5>
+								<span
+									class="h1"
+									>{{ $user->questions->count() }}</span
+								>
+								<br />
+								{{ str_plural('pedido', $user->questions->count()) }}
+								de socorro
+							</h5>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
