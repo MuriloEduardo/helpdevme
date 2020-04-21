7b2082d8d0ddec5dfe6af1a181f1f6ed9f620dc9
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2020-04-21 13:12:23 -0300

    muda redirecionamento

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
 	 *
 	 * @var string
 	 */
-	protected $redirectTo = '/questions/create?meta=conta-criada';
+	protected $redirectTo = '/dashboard?meta=conta-criada';
 
 	/**
 	 * Create a new controller instance.
